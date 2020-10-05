0064366bc551918ac818d0779bd4e04a4fc44c3b
Alberto0150 <albertosanjaya2@gmail.com>
2020-10-05 08:35:09 +0700

    new

---
+++ main.js
@@ -0,0 +1,26 @@
+function main(){
+    var canvas = document.getElementById("MyCanvas");
+    var gl = canvas.getContext("webgl");
+
+    var vertexShaderCode = `
+    void main() {
+
+    }
+    `;
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+
+    var fragmentShaderCode = `
+    void main(){
+
+    }
+    `;
+
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    
+}
