b1e7f4cdce93f8dfd94081b625d441cda091edeb
Alberto0150 <albertosanjaya2@gmail.com>
2020-10-13 19:37:40 +0700

    Revert "sampe triangles"
    This reverts commit cff716975d17cde06e0ad682df6e45651a244741.

--- main.js
+++ main.js
@@ -1,54 +1,52 @@
-function main() {
-    var canvas = document.getElementById("myCanvas");
+function main(){
+    var canvas = document.getElementById("MyCanvas");
     var gl = canvas.getContext("webgl");
-  
-    // Definisi verteks-verteks pada segitiga
+
+    // definisi verteks-verteks
     /**
-     * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5); D (0.5, 0.5)
+     * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5)
      */
-    var vertices = [
-      -0.5, 0.5,      // Titik A 
-      -0.5, -0.5,     // Titik B
-      0.5, -0.5,      // Titik C
-      0.5, -0.5,      // Titik C
-      0.5, 0.5,       // Titik D
-      -0.5, 0.5       // Titik A 
-    ];
-  
+    var vertices=[
+        -0.5, 0.5,  //point A
+        -0.5, -0.5, //point B
+        0.5, -0.5   // point C
+    ]
+
     var positionBuffer = gl.createBuffer();
+    // memberikan informasi ke global gpu buat position buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  
-    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
-  
+    //putus buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER,null);
+
+    var vertexShaderCode =document.getElementById("vertexShaderCode").text;
+
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
-  
-    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
-  
+
+    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;;
+
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
-  
+
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
-  
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer);
     var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
-  
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.viewport(100, 0, canvas.height, canvas.height);
-  
-    var primitive = gl.TRIANGLES;
+
+    var primitive = gl.LINE_LOOP;
     var offset = 0;
-    var count = 6;  // Jumlah verteks yang akan digambar
-    gl.drawArrays(primitive, offset, count);
-  }
+    var count =3; // jml vertex (titik sudut)
+    gl.drawArrays(primitive, offset ,count);
+}
