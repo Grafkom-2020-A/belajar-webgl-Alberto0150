7acd6310481ca2ad8ecd05d10df6e092af61bb6d
Alberto0150 <albertosanjaya2@gmail.com>
2020-10-13 21:49:47 +0700

    bug buat kotak dengan TRIANGLES yang beda warna

--- main.js
+++ main.js
@@ -7,20 +7,18 @@ function main(){
      * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5) ; D(0.5,0.5)
      */
     var vertices=[
-        -0.5, 0.5,  //point A
-        -0.5, -0.5, //point B
-        0.5, -0.5,  //point C
-        0.5, -0.5,  //point C
-        0.5, 0.5,   //point D
-        -0.5, 0.5,  //point A
-    ]
+        -0.5, 0.5, 1.0, 0.0, 0.0,  //point A
+        -0.5, -0.5, 1.0, 0.0, 0.0, //point B
+        0.5, -0.5, 1.0, 0.0, 0.0,  //point C
+        0.5, -0.5, 0.0, 0.0, 1.0,  //point C
+        0.5, 0.5, 0.0, 0.0, 1.0,  //point D
+        -0.5, 0.5, 0.0, 0.0, 1.0,  //point A
+    ];
 
-    var positionBuffer = gl.createBuffer();
-    // memberikan informasi ke global gpu buat position buffer
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var vertexBuffer = gl.createBuffer(); // memberikan informasi ke global gpu buat position buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    //putus buffer
-    gl.bindBuffer(gl.ARRAY_BUFFER,null);
+    gl.bindBuffer(gl.ARRAY_BUFFER,null); //putus buffer
 
     var vertexShaderCode =document.getElementById("vertexShaderCode").text;
 
@@ -40,17 +38,20 @@ function main(){
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer);
-    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_position");
+    var aColor = gl.getAttribLocation(shaderProgram, "a_color");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
     gl.enableVertexAttribArray(aPosition);
+    gl.enableVertexAttribArray(aColor);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.viewport(100, 0, canvas.height, canvas.height)
+    gl.viewport(100, 0, canvas.height, canvas.height);
 
     var primitive = gl.TRIANGLES;
     var offset = 0;
-    var count =6; // jml vertex (titik sudut)
+    var count = 6; // jml vertex (titik sudut)
     gl.drawArrays(primitive, offset ,count);
 }
